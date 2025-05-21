34ac23b74608ae87a31edc5b50e654a1b366f54a
Haye321 <hayea2480@gmail.com>
2025-05-21 20:59:39 +0500

    Addition

--- app.js
+++ app.js
@@ -2,17 +2,24 @@
 const express = require('express');
 const cors = require('cors');
 const searchRoutes = require('./routes/searchRoutes');
-const searchRankingRoutes = require('./routes/localRankingRoute');
+const searchRankingRoutes = require('./routes/locakRankingRoute');
 const chatRoutes = require('./routes/chatRoutes');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+const allowedOrigins = ['http://localhost:3000','https://seo-serp-frontend.vercel.app']; // Add your production frontend URL later
 app.use(cors({
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'OPTIONS','DELETE','PUT'], // Specify allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS','DELETE','PUT'], // Ensure OPTIONS is included
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Routes
